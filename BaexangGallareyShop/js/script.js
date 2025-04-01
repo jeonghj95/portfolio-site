c04afd25c22015659b1c07e29706ac0bccb35852
jeonghj95 <jeonghj95@gmail.com>
2025-04-01 11:36:34 +0900

    최신 파일 업로드

--- BaexangGallareyShop/js/script.js
+++ BaexangGallareyShop/js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+  // ART_DESIGN 슬라이드
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -17,6 +18,27 @@ $(function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // BEST_ART 슬라이드
+  var artSwiper = new Swiper(".artSwiper", {
+    slidesPerView: 4, 
+    spaceBetween: 20,  
+    // loop: true,       
+    pagination: {
+        el: ".art-pagination",
+        clickable: true,
+       },
+    navigation: {
+        nextEl: ".art-next",
+        prevEl: ".art-prev",
+    },
+    breakpoints: {
+        1440: { slidesPerView: 4 }, 
+        768: { slidesPerView: 3 },   
+        480: { slidesPerView: 2 },
+        320: { slidesPerView: 2 }   
+    }
+});
 
 });
 //ready
